3b46b4d24e5fc9df25b1ebdef4b91cf5eaed4d37
Santiago Borja Romero <santiago.borja@gmail.com>
2025-03-15 01:52:25 -0500

    FEAT: Revision de modulos deprecados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { Transmission } from './core/middleware/interceptor.middleware';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -16,11 +16,12 @@ import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule, 
-    HttpClientModule,
     BaseChartDirective,
+    HttpClientModule
   ],
   providers: [
     provideCharts(withDefaultRegisterables()),
+    provideHttpClient(withFetch()),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: HTTP_INTERCEPTORS , useClass: Transmission, multi: true }
   ],
