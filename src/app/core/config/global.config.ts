aa97ffa48291ee9a4a56037656c1443831e314f9
Santiago Borja Romero <santiago.borja@gmail.com>
2025-03-22 19:53:46 -0500

    DOC: Comentarios

--- src/app/core/config/global.config.ts
+++ src/app/core/config/global.config.ts
@@ -1,5 +1,5 @@
 export let Global = {
-    secret: "7PToGGTJ71knRd86WF39wfj619qewnbZ",
-    secret2: "q24nxK63oYShfXwU",
+    secret: "7PToGGTJ71knRd86WF39wfj619qewnbZ", // Key
+    secret2: "q24nxK63oYShfXwU", // Vector de cifrado
     prefix_storage: 'appdi_'
 };
